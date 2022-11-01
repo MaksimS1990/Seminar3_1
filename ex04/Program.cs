c2e8ead6e1a7c8c218163458fe7bf0102e730759
MaksimS <sipatovGT@mail.ru>
2022-11-01 10:13:50 +0400

    ex04tableSqrt

---
+++ ex04/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа на вход принимает одно число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1,4,9,16,25.
+// 2 -> 1,4.
+
+Console.WriteLine("Введите любое число: ");
+string writeN = Console.ReadLine()!;
+int N = Convert.ToInt32(writeN);;
+
+int[] GetSqrtTable (int N)
+{
+    int[] SqrtArray = new int [N];
+
+    for (int i = 0, number = 1; i < N; i++, number++)
+    {
+        SqrtArray[i] = number * number;
+    }
+    return SqrtArray;
+}
+
+void PrintTable(int[] Array)
+{
+    int number = 1;
+    for(int i = 0; i < Array.Length; i++)
+    {
+        Console.WriteLine(number + " ^ 2 = " + Array[i]);
+        number++;
+    }
+}
+
+int[] SqrtTable = GetSqrtTable(N);
+PrintTable(SqrtTable);
